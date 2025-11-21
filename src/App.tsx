9132ac65357c7125825ba6428cc6cbc49be0e1c8
JesusPinzon-Sainapsis <jesus.vega@sainapsis.com>
2025-11-21 09:48:27 -0500

    feat: Add AuthProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
+import { AuthProvider } from '@/context/AuthContext';
 import { GameProvider } from '@/context/GameContext';
 import { AppRoutes } from '@/routes/AppRoutes';
 import './App.css';
 
 function App() {
-  return (
-      <GameProvider>
-        <AppRoutes />
-      </GameProvider>
-  );
+    return (
+        <AuthProvider>
+            <GameProvider>
+                <AppRoutes />
+            </GameProvider>
+        </AuthProvider>
+    );
 }
 
 export default App;
